f3cdbfac17d00e5c40baa139bbc2b9c9ad88793c
Eugane Kulakovski <kulakovsky@strateg.by>
2021-10-22 14:15:19 +0300

    initial commit

---
+++ apps/ph-video/jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  displayName: 'ph-video',
+  preset: '../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+    },
+  },
+  coverageDirectory: '../../coverage/apps/ph-video',
+  transform: {
+    '^.+\\.(ts|js|html)$': 'jest-preset-angular',
+  },
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
+};
